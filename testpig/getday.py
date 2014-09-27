1d6e31061a1524fdd7a703cd76a36a13385046e5
Ubuntu <ubuntu@ip-172-31-27-191.us-west-1.compute.internal>
2014-09-27 03:48:45 +0000

    Removed duplicated directory

--- testpig/getday.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def getday(year, month, day):
-  if len(month) < 2: month = "0"+month
-  if len(day) < 2: day = "0"+day
-  return year+month+day
-
-if __name__ == "__main__":
-  print getday(sys.argv[1], sys.argv[2], sys.argv[3])
