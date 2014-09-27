291cf9aa5e56e642fc1b46a5a7a8fa917b7e1ca8
Ubuntu <ubuntu@ip-172-31-27-191.us-west-1.compute.internal>
2014-09-27 01:26:04 +0000

    updating rudimentary UI

--- testpig/testflask/app/views.py
+++ testpig/testflask/app/views.py
@@ -3,61 +3,65 @@ from flask import render_template, request, redirect
 from app import app
 import happybase
 connection = happybase.Connection()
-wvel = connection.table('allwindskinny3')
+wvel = connection.table('allwindskinny')
 
 region_data = { \
+  'GB': {'name': 'Great Britain', \
+    'latmin': 50.0, 'latmax': 58.0, \
+    'longmin': 349.5, 'longmax': 360.0, \
+    'lostep': 1.0, 'center': (53.5, 356)}, \
   'CONUS': {'name': "Continental U.S.", \
     'latmin': 25.5, 'latmax': 49.5, \
     'longmin': 235, 'longmax': 293.5, \
-    'step': 0.5, 'center': (38.0, 263.0)}, \
+    'lostep': 0.5, 'center': (38.0, 263.0)}, \
   'EUSO': {'name': 'Europe - South', \
     'latmin': 25.5, 'latmax': 49.5, \
     'longmin': 235, 'longmax': 293.5, \
-    'step': 0.5, 'center': (38.0, 263.0)}, \
+    'lostep': 0.5, 'center': (38.0, 263.0)}, \
   'EUNO': {'name': 'Europe - Nordic', \
     'latmin': 25.5, 'latmax': 49.5, \
     'longmin': 235, 'longmax': 293.5, \
-    'step': 0.5, 'center': (38.0, 263.0)}, \
+    'lostep': 0.5, 'center': (38.0, 263.0)}, \
   'AFNO': {'name': 'Africa - North', \
     'latmin': 25.5, 'latmax': 49.5, \
     'longmin': 235, 'longmax': 293.5, \
-    'step': 0.5, 'center': (38.0, 263.0)}, \
+    'lostep': 0.5, 'center': (38.0, 263.0)}, \
   'AFSO': {'name': 'Africa - South', \
     'latmin': 25.5, 'latmax': 49.5, \
     'longmin': 235, 'longmax': 293.5, \
-    'step': 0.5, 'center': (38.0, 263.0)}, \
+    'lostep': 0.5, 'center': (38.0, 263.0)}, \
   'ALA': {'name': 'Alaska and Yukon', \
     'latmin': 25.5, 'latmax': 49.5, \
     'longmin': 235, 'longmax': 293.5, \
-    'step': 0.5, 'center': (38.0, 263.0)}, \
+    'lostep': 0.5, 'center': (38.0, 263.0)}, \
   'CAN': {'name': 'Canada', \
     'latmin': 25.5, 'latmax': 49.5, \
     'longmin': 235, 'longmax': 293.5, \
-    'step': 0.5, 'center': (38.0, 263.0)}, \
+    'lostep': 0.5, 'center': (38.0, 263.0)}, \
   'CAM': {'name': 'Central America', \
     'latmin': 1.0, 'latmax': 24.5, \
     'longmin': 252.0, 'longmax': 311.0, \
-    'step': 0.5, 'center': (38.0, 263.0)}, \
+    'lostep': 0.5, 'center': (38.0, 263.0)}, \
   'SSA': {'name': 'South America', \
     'latmin': -36.0, 'latmax': -0.5, \
     'longmin': 277.0, 'longmax': 325.5, \
-    'step': 0.5, 'center': (38.0, 263.0)}, \
+    'lostep': 0.5, 'center': (38.0, 263.0)}, \
   'AUS': {'name': 'Australia', \
     'latmin': -44.0, 'latmax': -9.0, \
     'longmin': 112.0, 'longmax': 156.5, \
-    'step': 0.5, 'center': (-26.0, 134.5)}, \
+    'lostep': 0.5, 'center': (-26.0, 134.5)}, \
   'ME': {'name': 'Middle East', \
     'latmin': 14.0, 'latmax': 41.0, \
     'longmin': 24.5, 'longmax': 77.5, \
-    'step': 0.5, 'center': (29.0, 52.0)}, \
+    'lostep': 0.5, 'center': (29.0, 52.0)}, \
   'SEA': {'name': 'Asia - Southeast', \
     'latmin': 6.0, 'latmax': 34.5, \
     'longmin': 70.0, 'longmax': 126.0, \
-    'step': 0.5, 'center': (23.5, 98.0)}, \
+    'lostep': 0.5, 'center': (23.5, 98.0)}, \
   'NEA': {'name': 'Asia: Northeast', \
     'latmin': 29.5, 'latmax': 52.5, \
     'longmin': 89, 'longmax': 147.5, \
-    'step': 0.5, 'center': (41.5, 114.5)}, \
+    'lostep': 0.5, 'center': (41.5, 114.5)}, \
 }
 
 @app.route('/')
@@ -80,10 +84,11 @@ def showheatmap(region, day):
   latmax =  region_data[region]['latmax']
   longmin = region_data[region]['longmin']
   longmax =  region_data[region]['longmax']
-  step = region_data[region]['step']
+  lastep = 0.5
+  lostep = region_data[region]['lostep']
   center = region_data[region]['center']
   vel_in = loadarea(latmin, latmax, longmin, \
-	longmax, step, day)
+	longmax, lostep, day)
   regions = [(reg, region_data[reg]['name']) \
         for reg in region_data]
   return render_template("loadarea.html",
@@ -92,10 +97,10 @@ def showheatmap(region, day):
 	title = "Wind Speed in Meters per Second",
 	regions = regions) 
 
-def loadarea(latmin, latmax, longmin, longmax, step, day):
+def loadarea(latmin, latmax, longmin, longmax, lostep, day):
   vel_in = [] 
-  las = arange(latmin, latmax, step)
-  los = arange(longmin, longmax, step)
+  las = arange(latmin, latmax, 0.5)
+  los = arange(longmin, longmax, lostep)
   lalo = zip(repeat(las, len(los)),tile(los, len(las)))
   for (la, lo) in lalo:
     row = wvel.row(day+'_'+str(la)+'_'+str(lo))
